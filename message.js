9e2ac50edb84c43affbbe0b398bcf5d421d87093
Thomas <lainonuma@gmail.com>
2019-06-03 22:22:11 +0200

    Loli Search
    Just a loli search site

---
+++ message.js
@@ -0,0 +1,22 @@
+﻿var list = 
+	["Yoshihiro Togashi cioè il creatore del manga Yu Degli Spetri e di Hunter x Hunter,  è sposato con la creatrice del manga Sailor Moon ossia Naoko Takeuki",
+	"Nel celebre anime chiamato Dragonball, due dei protagonisti hanno nomi particolari, si tratta di Gohan e Pan, questi nomi in giapponese vogliono dire riso e pane",
+	"Inizialmente Goku doveva essere una scimmia",
+	"La figlia di Crilin, che si chiama Marron non ha il naso in Dragonball",
+	"La voce di Rin e Len, fratelli Vocaloid 02 vengono doppiati da una singola persona",
+	"L'idea di pokèmon, deriva dalla passione nel collezionare insetti dell'autore",
+	"Il primo Gennaio 1963 è stato messo in onda in bianco e nero il primo anime della storia realizzato da Osamu Tezuka. Il nome dell'anime in questione è Astro Boy",
+	"Sazae-san è l'anime più longevo della storia. La prima puntata è stata messa in onda nel 1969 ed è tutt'ora in corso",
+	"In Giappone escono una media di 40 nuovi anime alla settimana",
+	"Originariamente la seconda serie di Dragonball si doveva chiamare Dragonball 2 ma per un errore di lettura si scambiò il numero 2 per una Z ed è per questo che la seconda stagione ha questo nome",
+	"In Dragonball, Vegeta, temibile Principe dei Saiyan ha una paura particolare, soffre infatti di scolecifobia. Cioè paura dei vermi",
+	"Akira Toriyama il creatore di Dragonball, per creare Goku fu ispirato da un libro cinese Viaggio in Occidente dove il protagonista era un semidio mezzo uomo mezzo scimmia che viaggiava a bordo di una nuvola sacra dorata e combatteva con un bastone allungabile",
+	"Hajime Isayama ideatore di Attack on Titan, ha avuto la sua idea dopo aver visto un ubriaco in un bar, infatti se osserva attentamente si può notare che i titani corrono come gli ubriachi",
+	"Nel 1971, lo studio Ghibli (noto studio cinematografico di film d'animazione) voleva pubblicare un anime ispirato a Pippi calzelunge",
+	"Il pianeta Namek in Dragonball è illuminato da 3 soli pertanto non cala mai la notte",
+	"Goku è stato scelto come ambasciatore delle olimpiadi di Tokyo del 2020",
+	"Nel manga di Dragonball, il serpentone è lungo 1 milione di chilometri. Nel cartone invece, è lungo solo 100 chilometri"]
+
+
+var index = Math.floor(Math.random() * list.length)
+	document.write(list[index])
